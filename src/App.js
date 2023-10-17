d0b9d47938a7f8bfbc662208bee2b0a434373151
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-10-18 06:43:28 +0700

    update img layout

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h2 className='
           font-bold
-          text-2xl
+          text-3xl
           text-center
           text-blue-400
         '>
