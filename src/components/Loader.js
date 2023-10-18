a3576f330434f9e43c1daf17a2b35150df7a2fbc
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-10-18 15:12:29 +0700

    update loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loader = () => {
   return (
-    <div className="flex justify-center items-center h-full">
+    <div className="flex justify-center items-center h-full mt-5">
       <div className="border-t-4 border-gray-300 rounded-full animate-spin w-8 h-8"></div>
     </div>
   );
