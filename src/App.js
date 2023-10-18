276118a28b7fcd93d8b569a4d0c9157497dfa4e5
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-10-18 10:32:17 +0700

    update app layout

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import ImageList from './components/ImageList';
 
 function App() {
   const [images, setImages] = useState([]);
-  const [searchTerm, setSearchTerm] = useState(""); // Step 1
+  const [searchTerm, setSearchTerm] = useState("");
 
-  const onSearchSubmit = async () => { // Step 3
-    const result = await searchImages(searchTerm); // Use searchTerm
+  const onSearchSubmit = async () => { 
+    const result = await searchImages(searchTerm);
     setImages(result);
   }
 
@@ -24,11 +24,12 @@ function App() {
         <input 
           type='text' 
           className='border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none'
-          value={searchTerm} // Step 2
-          onChange={(e) => setSearchTerm(e.target.value)} // Step 2
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder='Search Image'
         />
         <button 
-          className='ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+          className='ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out'
           onClick={onSearchSubmit}
         >
           Search
