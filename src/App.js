5c53d4d23e29fefd8f2cc9c162981380afdc04f0
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-10-18 15:50:35 +0700

    update app layout

--- src/App.js
+++ src/App.js
@@ -35,49 +35,45 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo mt-5" alt="logo" />
-        <h2 className='font-bold text-3xl text-center text-white'>
-          React Image Generator
-        </h2>
-        
-        <div className='flex justify-center items-center mt-10'>
-          <input 
-            type='text' 
-            className='border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none'
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            onKeyPress={handleKeyPress}
-            placeholder='Search Image'
+  <header className="App-header">
+    <img src={logo} className="App-logo mt-5" alt="logo" />
+    <h2 className="font-bold text-3xl text-center text-white">
+      React Image Generator
+    </h2>
+
+    <div className="flex flex-col items-center mt-10">
+      <input
+        type="text"
+        className="input-search border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none w-full max-w-md"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        onKeyPress={handleKeyPress}
+        placeholder="Search Image"
+      />
+      <div className="mt-2 flex justify-center space-x-2">
+        <button
+          className="button-search bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out"
+          onClick={onSearchSubmit}
+        >
+          Search
+        </button>
+        <button
+          className="button-refresh bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out"
+        >
+          <FiRefreshCw
+            className="text-2xl"
+            onClick={() => window.location.reload()}
           />
-          <button 
-            className='ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out'
-            onClick={onSearchSubmit}
-          >
-            Search
-          </button>
-          <button className='
-            ml-2 
-            bg-gray-500 
-            hover:bg-gray-700 
-            text-white 
-            font-bold 
-            py-2 
-            px-4 
-            rounded 
-            transition 
-            duration-300 
-            ease-in-out
-          '>
-            <FiRefreshCw className='text-2xl' onClick={() => window.location.reload()} />
-          </button>
-        </div>
-        {loading && <Loader />}
-        {searched && images.length === 0 && !loading && <NoImage />}
-        {images.length > 0 && <ImageList images={images} />}
-        <Copyright />
-      </header>
+        </button>
+      </div>
     </div>
+    {loading && <Loader />}
+    {searched && images.length === 0 && !loading && <NoImage />}
+    {images.length > 0 && <ImageList images={images} />}
+    <Copyright />
+  </header>
+</div>
+
   );
 }
 
