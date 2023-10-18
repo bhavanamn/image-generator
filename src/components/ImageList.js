b9a7ab49e9f913d3d4fbe4abd72a41b1daeb1803
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-10-18 10:49:17 +0700

    update image show layout

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -9,15 +9,14 @@ const ImageList = ({
     })
   return (
     <div className='
-        grid 
-        grid-cols-1 
-        md:grid-cols-2 
-        lg:grid-cols-3 
-        gap-4 
-        mt-10 
-        mb-10
-        w-4/5
-        h-auto
+        grid
+        grid-cols-1
+        md:grid-cols-2
+        lg:grid-cols-3
+        gap-4
+        mt-5
+        mb-5
+
     '>
       {renderedImages}
     </div>
