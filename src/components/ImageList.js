91b7d4a11cae63fd903ae342b6a23eec58de2847
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-10-18 15:05:40 +0700

    update image list layout

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,26 +1,22 @@
-import React from 'react'
-import ImageShow from './ImageShow'
+import React from 'react';
+import ImageShow from './ImageShow';
 
-const ImageList = ({
-    images
-}) => {
-    const renderedImages = images.map((image) => {
-        return <ImageShow key={image.id} image={image} />
-    })
+const ImageList = ({ images }) => {
+  const renderedImages = images.map((image) => {
+    return <ImageShow key={image.id} image={image} />;
+  });
+
   return (
-    <div className='
-        grid
-        grid-cols-1
-        md:grid-cols-2
-        lg:grid-cols-3
-        gap-4
-        mt-5
-        mb-5
-
-    '>
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-5 mb-5'>
       {renderedImages}
+      <style jsx>{`
+        .grid img {
+          width: 100%; 
+          height: auto;
+        }
+      `}</style>
     </div>
-  )
-}
+  );
+};
 
-export default ImageList
+export default ImageList;
