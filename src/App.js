d631f6314eeafdc1f67b070ab9ae14e190afd57d
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-10-18 10:53:04 +0700

    update app layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import React, { useState } from 'react';
+import ImageList from './components/ImageList';
+import NoImage from './components/NoImage';
+import searchImages from './api/api';
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-import searchImages from './api/api';
-import ImageList from './components/ImageList';
 
 function App() {
   const [images, setImages] = useState([]);
@@ -21,23 +22,22 @@ function App() {
           React Image Generator
         </h2>
         <div className='flex justify-center items-center mt-10'>
-        <input 
-          type='text' 
-          className='border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none'
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder='Search Image'
-        />
-        <button 
-          className='ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out'
-          onClick={onSearchSubmit}
-        >
-          Search
-        </button>
-      </div>
-      <ImageList images={images} />
+          <input 
+            type='text' 
+            className='border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder='Search Image'
+          />
+          <button 
+            className='ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out'
+            onClick={onSearchSubmit}
+          >
+            Search
+          </button>
+        </div>
+        {images.length === 0 ? <NoImage /> : <ImageList images={images} />}
       </header>
-      
     </div>
   );
 }
---
+++ src/components/NoImage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const NoImage = () => {
+  return (
+    <div className='
+        flex
+        justify-center
+        items-center
+        mt-10
+        mb-10
+        text-white
+        text-2xl
+        font-bold
+    '>
+      No Image Found
+    </div>
+  )
+}
+
+export default NoImage
