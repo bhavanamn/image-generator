7fb7c4ac259b2a21b00a2a8fecc5f7c3af9da4ed
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-10-27 18:17:54 +0700

    update env

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
+
+const clientId = process.env.REACT_APP_UNSPLASH_CLIENT_ID;
 
 const searchImages = async (term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
         headers : {
-            Authorization: "Client-ID eQq_lpkzH6EapRy2sKpCc-LSG2tDqyuSIqUMKzm8gwE",
+            Authorization: `Client-ID ${clientId}`,
         },
         params: {
             query: term
